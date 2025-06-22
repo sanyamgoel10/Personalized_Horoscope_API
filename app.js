2ccbb8e84e0b7ca0e2b078edc3adfdcf354c0e5e
Sanyam Goel <sanyamgoel10@gmail.com>
2025-06-22 15:11:06 +0530

    rate limiter added

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Rate Limiter Added in all APIs
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5, // 5 requests per minute allowed from a single IP address
+  message: {
+    msg: 'Too many requests, please try again after a minute.'
+  }
+});
+
 // Middleware
 app.use(express.json());
 const AuthMiddleware = require('./middlewares/authMiddleware.js');
@@ -9,6 +19,7 @@ const AuthMiddleware = require('./middlewares/authMiddleware.js');
 const HoroscopeRoutes = require('./routes/horoscopeRoutes.js');
 const HomeRoutes = require('./routes/homeRoutes.js');
 
+app.use(limiter);
 app.use('/', HomeRoutes);
 app.use('/horoscope', AuthMiddleware.validateJwtToken, HoroscopeRoutes);
 
